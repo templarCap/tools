b159bcb8cbce9132ad403e4974c380f2f48331c3
akzo <anarkazimov98@hotmail.com>
2015-08-25 21:51:36 +0500

    1.0

---
+++ idlelib/idlever.py
@@ -0,0 +1,5 @@
+"""Unused by Idle: there is no separate Idle version anymore.
+Kept only for possible existing extension use."""
+from sys import version
+IDLE_VERSION = version[:version.index(' ')]
+
